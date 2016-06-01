46d4a8ce262c0bba9c6b35ec060d6ad402f9b14f
HarryPaek <paek.harry@gmail.com>
2016-06-01 13:51:32 +0900

    Fixed for file size limitation

--- WebAPIFileUpload/WebAPIFileUpload.WebAPI/Controllers/FileUploadController.cs
+++ WebAPIFileUpload/WebAPIFileUpload.WebAPI/Controllers/FileUploadController.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -14,22 +15,37 @@ namespace WebAPIFileUpload.WebAPI.Controllers
         [MimeMultipart]
         public async Task<FileUploadResult> Post()
         {
-            var uploadPath = HttpContext.Current.Server.MapPath("~/Uploads");
-
-            var multipartFormDataStreamProvider = new UploadMultipartFormProvider(uploadPath);
-
-            // Read the MIME multipart asynchronously 
-            await Request.Content.ReadAsMultipartAsync(multipartFormDataStreamProvider);
-
-            string _localFileName = multipartFormDataStreamProvider.FileData.Select(multiPartData => multiPartData.LocalFileName).FirstOrDefault();
-
-            // Create response
-            return new FileUploadResult
+            try
             {
-                LocalFilePath = _localFileName,
-                FileName = Path.GetFileName(_localFileName),
-                FileLength = new FileInfo(_localFileName).Length
-            };
+                var uploadPath = HttpContext.Current.Server.MapPath("~/Uploads");
+
+                var multipartFormDataStreamProvider = new UploadMultipartFormProvider(uploadPath);
+
+                // Read the MIME multipart asynchronously 
+                await Request.Content.ReadAsMultipartAsync(multipartFormDataStreamProvider);
+
+                string _localFileName = multipartFormDataStreamProvider.FileData.Select(multiPartData => multiPartData.LocalFileName).FirstOrDefault();
+
+                // Create response
+                return new FileUploadResult
+                {
+                    LocalFilePath = _localFileName,
+                    FileName = Path.GetFileName(_localFileName),
+                    FileLength = new FileInfo(_localFileName).Length
+                };
+            }
+            catch(HttpException httpex)
+            {
+                throw httpex;
+            }
+            catch(IOException ioex)
+            {
+                throw ioex;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
--- WebAPIFileUpload/WebAPIFileUpload.WebAPI/app/controllers/fileUploadCtrl.js
+++ WebAPIFileUpload/WebAPIFileUpload.WebAPI/app/controllers/fileUploadCtrl.js
@@ -2,13 +2,20 @@
 
 angular.module('angularUploadApp')
     .controller('fileUploadCtrl', function ($scope, $http, $timeout, Upload) {
+        $scope.progress = 0;
+        $scope.sizeLimit = 2147483647; //About 2GB Size Limit
         $scope.upload = [];
         $scope.UploadedFiles = [];
 
         $scope.startUploading = function ($files) {
+            $scope.UploadingFiles = [];
             //$files: an array of files selected
             for (var i = 0; i < $files.length; i++) {
                 var $file = $files[i];
+
+                $scope.UploadingFiles.push({Name: $file.name, Progress: 0});
+                $scope.progress = 0;
+
                 (function (index) {
                     $scope.upload[index] = Upload.upload({
                         url: "./api/fileupload", // webapi url
@@ -17,6 +24,7 @@ angular.module('angularUploadApp')
                     })
                     .progress(function (evt) {
                         $scope.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+                        $scope.UploadingFiles[index].Progress = $scope.progress;
                     })
                     .success(function (data, status, headers, config) {
                         // file is uploaded successfully
